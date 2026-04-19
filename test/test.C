39db1a91583c1dfd2c895be41a56b6e6abca5865
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2026-04-19 09:12:31 -0500

    Add cosmic file list

--- test/test.C
+++ test/test.C
@@ -7,13 +7,15 @@ int test(Long64_t max_entries = 1e4, Long64_t first_entry = 0) {
   // TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.CeEndpointOnSpillTriggered.MDC2020aq_best_v1_3_v06_03_00.root.files";
   // TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.CosmicCRYSignalAllOnSpillTriggered.MDC2020aq_best_v1_3_v06_03_00.root.files";
   // TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.RPCExternalOnSpillTriggered.MDC2020aq_best_v1_3_v06_03_00.root.files";
-  TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.CeEndpointMix1BBTriggered.MDC2025-000.root.files";
+  // TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.CeEndpointMix1BBTriggered.MDC2025-000.root.files";
+  TString file_list = "Mu2eEvtAna/file_lists/nts.mu2e.CosmicSignalOnSpillTriggered.MDC2025-001.root.files";
 
   // Setup a Mu2eEvtAna processing
   gAna = new Mu2eEvtAna::Mu2eEvtAna((max_entries < 10) ? (max_entries == 1) ? 10 : 2 : 0);
   gAna->AddFile(file_list, max_entries, first_entry);
   gAna->SetName("test");
   gAna->cache_size_ = 2000000000U; //2 GB cache
+  gAna->load_baskets_ = false; // whether or not to load tree baskets when a new file is opened
 
   // Process the tree
   const int status = gAna->Process(max_entries);
